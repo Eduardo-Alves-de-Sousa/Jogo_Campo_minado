aec4259161c20bbd8462b598b56cec7b479023e6
Eduardo-Alves-de-Sousa <eduardoalvessousa031@gmail.com>
2023-09-28 00:22:37 -0300

    Criação dos componentes e aplicação na tela app para iniciação do jogo

--- campo_minado/lib/screens/campo_minado_app.dart
+++ campo_minado/lib/screens/campo_minado_app.dart
@@ -1,17 +1,24 @@
 //import 'package:flutter/foundation.dart';
 //import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
+import '../components/resultado_widget.dart';
 
 class CampoMinadoApp extends StatelessWidget {
   const CampoMinadoApp({super.key});
+
+  _reiniciar() {
+    print('reiniciar...');
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Campo Minado'),
+        appBar: ResultadoWidget(
+          venceu: true,
+          onReiniciar: _reiniciar,
         ),
+        // ignore: avoid_unnecessary_containers
         body: Container(
           child: const Text('Tabuleiro'),
         ),
